867fabeb0b623a30b7739ac7148055e5fe778c39
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-09-13 12:25:19 -0700

    more exercism algo practice

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,22 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
     // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int timeCooked) {
+        return expectedMinutesInOven() - timeCooked;
+    }
 
     // TODO: define the 'preparationTimeInMinutes()' method
-
+    public int preparationTimeInMinutes(int layers) {
+        int prepMinutesPerLayer = 2;
+        return prepMinutesPerLayer * layers;
+    }
     // TODO: define the 'totalTimeInMinutes()' method
+
+    public int totalTimeInMinutes(int layers, int timeCooked) {
+        return preparationTimeInMinutes(layers) + timeCooked;
+    }
 }
